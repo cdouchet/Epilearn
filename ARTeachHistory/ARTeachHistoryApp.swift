a6bee06eef0e6e694313c910b12afd75357f74c7
Cyril <cyril.douchet@gmail.com>
2023-10-05 17:06:05 +0200

    Initial Commit

---
+++ ARTeachHistory/ARTeachHistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARTeachHistoryApp.swift
+//  ARTeachHistory
+//
+//  Created by Cyril DOUCHET on 05/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct ARTeachHistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
